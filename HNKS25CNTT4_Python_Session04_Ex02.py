62c8c14a763ee35d0a717c5cc2928bfb29405c7f
DansuraitoAhiru <vqviet2007@gmail.com>
2026-05-21 07:04:47 +0700

    Update HNKS25CNTT4_Python_Session04_Ex02.py

--- HNKS25CNTT4_Python_Session04_Ex02.py
+++ HNKS25CNTT4_Python_Session04_Ex02.py
@@ -1,11 +1,13 @@
 count = 0
+total_revenue = 0
 for i in range(1, 8):
     revenue = int(input(f"Nhập doanh thu Ngày {i}: "))
 
     if revenue >= 5000000:
         count+=1
     
+    total_revenue += revenue
 print("--- BÁO CÁO DOANH THU TUẦN RIKKEI STORE ---")
-print(f"Tổng doanh thu cả tuần: {revenue}")
-print(f"Doanh thu trung bình mỗi ngày: {revenue/7}")
+print(f"Tổng doanh thu cả tuần: {total_revenue}")
+print(f"Doanh thu trung bình mỗi ngày: {(total_revenue/7):.2f}")
 print(f"Số ngày đạt doanh thu mục tiêu (>= 5000000): {count} ngày")
